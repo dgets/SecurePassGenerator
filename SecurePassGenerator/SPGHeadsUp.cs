2b23aaaa91f2d2ff72ba179913bafaa592f0dd0a
Damon Getsman <damo.gets@gmail.com>
2018-08-26 12:32:14 -0500

    Completed GUI components
    The CLB is now populating correctly, as well.  Forgot how the flowchart went when initializing a new c# 'doze forms application, I guess...

--- SecurePassGenerator/SPGHeadsUp.cs
+++ SecurePassGenerator/SPGHeadsUp.cs
@@ -11,24 +11,24 @@ using System.Windows.Forms;
 namespace SecurePassGenerator
 {
     public partial class PWGMain : Form {
-        private Dictionary<string, bool> charOpts;
-
         public PWGMain() {
-            //global level init
-            charOpts = new Dictionary<string, bool> { { "alphabetic", true }, 
-                { "upper", false }, { "numeric", true }, { "symbolic", false } };
+            //init
+            Dictionary<string, bool> charOpts = 
+                new Dictionary<string, bool> { { "alphabetic", true }, 
+                    { "upper", false }, { "numeric", true }, 
+                    { "symbolic", false } };
+
+            InitializeComponent();
 
             //GUI init
             initClbCharOpts(charOpts);
-
-            InitializeComponent();
         }
 
         //public static Dictionary<string, bool> CharOpts { get => charOpts; set => charOpts = value; }
 
         private void initClbCharOpts(Dictionary<string, Boolean> optsSet) {
-            foreach (string charOption in charOpts.Keys) {
-                if (charOpts[charOption] == true) {
+            foreach (string charOption in optsSet.Keys) {
+                if (optsSet[charOption] == true) {
                     clbCharOpts.Items.Add(charOption, true);
                 } else {
                     clbCharOpts.Items.Add(charOption, false);

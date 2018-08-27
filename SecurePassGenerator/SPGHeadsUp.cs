d1b8ab5561b12eede4e2dffa3a1954a184cac2b8
Damo Gets <dgets@users.noreply.github.com>
2018-08-27 06:31:03 -0500

    Restructured UI code due to some strange repository syncing issues.

--- SecurePassGenerator/SPGHeadsUp.cs
+++ SecurePassGenerator/SPGHeadsUp.cs
@@ -11,28 +11,22 @@ using System.Windows.Forms;
 namespace SecurePassGenerator
 {
     public partial class PWGMain : Form {
-        private Dictionary<string, bool> charOpts;
+        //settings
+        private Dictionary<string, bool> charOpts = new Dictionary<string, bool>
+            { { "alphabetic", true }, { "upper", false }, { "numeric", true }, 
+              { "symbolic", false } };
 
         public PWGMain() {
-            //global level init
-            charOpts = new Dictionary<string, bool> { { "alphabetic", true }, 
-                { "upper", false }, { "numeric", true }, { "symbolic", false } };
-
-            //GUI init
-            initClbCharOpts(charOpts);
-
             InitializeComponent();
+
+            //global level init
+            //GUI init
+            initClbCharOpts();
         }
 
-        //public static Dictionary<string, bool> CharOpts { get => charOpts; set => charOpts = value; }
-
-        private void initClbCharOpts(Dictionary<string, Boolean> optsSet) {
+        private void initClbCharOpts() {
             foreach (string charOption in charOpts.Keys) {
-                if (charOpts[charOption] == true) {
-                    clbCharOpts.Items.Add(charOption, true);
-                } else {
-                    clbCharOpts.Items.Add(charOption, false);
-                }
+                clbCharOpts.Items.Add(charOption, charOpts[charOption]);
             }
         }
     }
